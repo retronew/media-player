af2531c212c02dd4383ed03cc965f285dbc3c776
retronew <7346148+retronew@users.noreply.github.com>
2025-07-28 18:10:07 +0800

    feat: updated size

--- src/components/VueMediaPlayer.vue
+++ src/components/VueMediaPlayer.vue
@@ -6,8 +6,8 @@ import 'media-chrome'
 
 const props = withDefaults(defineProps<Props>(), {
   poster: '',
-  width: 704,
-  height: 528,
+  width: '100%',
+  height: undefined,
   preload: 'auto',
   controls: true,
   muted: false,
@@ -191,153 +191,158 @@ onMounted(() => {
 onUnmounted(() => {
   document.removeEventListener('fullscreenchange', handleFullscreenChange)
 })
+
+function isNumber(value: unknown): value is number {
+  return typeof value === 'number' && !Number.isNaN(value) && Number.isFinite(value)
+}
 </script>
 
 <template>
-  <media-controller
-    ref="mediaControllerRef"
-    class=":uno: group relative col-span-full row-span-full w-full flex overflow-hidden rounded-xl"
-    :class="[
-      containerClass,
-    ]"
-    role="region"
-    aria-label="video player"
-    :media-muted="muted"
-    :media-volume="volume"
-    :media-current-time="currentTime"
-    :media-duration="duration"
-    :media-playback-rate="playbackRate"
-    @media-play-request="handlePlay"
-    @media-pause-request="handlePause"
-    @media-seek-request="handleSeek"
-    @media-volume-request="handleVolumeChange"
-    @media-mute-request="handleMute"
-    @media-unmute-request="handleUnmute"
-    @media-fullscreen-request="handleFullscreen"
-    @media-fullscreen-exit-request="handleExitFullscreen"
+  <div
+    :style="{
+      width: isNumber(width) ? `${width}px` : width,
+      height: isNumber(height) ? `${height}px` : height,
+    }"
   >
-    <video
-      slot="media"
-      ref="videoRef"
-      class=":uno: my-0"
+    <media-controller
+      ref="mediaControllerRef"
+      class=":uno: group relative col-span-full row-span-full w-full flex overflow-hidden rounded-xl"
       :class="[
-        videoClass,
-        {
-          width,
-          height,
-        },
+        containerClass,
       ]"
-      :src="src"
-      :poster="poster"
-      :preload="preload"
-      :muted="muted"
-      :autoplay="autoplay"
-      :loop="loop"
-      :crossorigin="crossorigin"
-      :playsinline="playsinline"
-      tabindex="-1"
-      @loadedmetadata="handleLoadedMetadata"
-      @timeupdate="handleTimeUpdate"
-      @durationchange="handleDurationChange"
-      @volumechange="handleVolumeUpdate"
-      @play="handleVideoPlay"
-      @pause="handleVideoPause"
-      @ended="handleEnded"
-      @error="handleError"
-    />
-
-    <div
-      v-if="controls"
-      class=":uno: absolute inset-x-0 bottom-0 px-4 pb-4 md:px-12"
-      :class="[
-        controlsContainerClass,
-      ]"
+      role="region"
+      aria-label="video player"
+      :media-muted="muted"
+      :media-volume="volume"
+      :media-current-time="currentTime"
+      :media-duration="duration"
+      :media-playback-rate="playbackRate"
+      @media-play-request="handlePlay"
+      @media-pause-request="handlePause"
+      @media-seek-request="handleSeek"
+      @media-volume-request="handleVolumeChange"
+      @media-mute-request="handleMute"
+      @media-unmute-request="handleUnmute"
+      @media-fullscreen-request="handleFullscreen"
+      @media-fullscreen-exit-request="handleExitFullscreen"
     >
-      <media-control-bar
-        class=":uno: h-12 w-full overflow-hidden border border-border rounded-xl border-solid bg-background/98 px-1 shadow-[0_0.25rem_0.625rem_0rem_hsla(var(--mp-background)/.2)] md:px-2.5"
+      <video
+        slot="media"
+        ref="videoRef"
+        class=":uno: my-0"
+        :class="videoClass"
+        :src="src"
+        :poster="poster"
+        :preload="preload"
+        :muted="muted"
+        :autoplay="autoplay"
+        :loop="loop"
+        :crossorigin="crossorigin"
+        :playsinline="playsinline"
+        tabindex="-1"
+        @loadedmetadata="handleLoadedMetadata"
+        @timeupdate="handleTimeUpdate"
+        @durationchange="handleDurationChange"
+        @volumechange="handleVolumeUpdate"
+        @play="handleVideoPlay"
+        @pause="handleVideoPause"
+        @ended="handleEnded"
+        @error="handleError"
+      />
+
+      <div
+        v-if="controls"
+        class=":uno: absolute inset-x-0 bottom-0 px-4 pb-4 md:px-12"
         :class="[
-          controlBarClass,
+          controlsContainerClass,
         ]"
       >
-        <media-play-button
-          class=":uno: p-2.5 outline-none"
-          :aria-label="isPlaying ? 'pause' : 'play'"
+        <media-control-bar
+          class=":uno: h-12 w-full overflow-hidden border border-border rounded-xl border-solid bg-background/98 px-1 shadow-[0_0.25rem_0.625rem_0rem_hsla(var(--mp-background)/.2)] md:px-2.5"
+          :class="[
+            controlBarClass,
+          ]"
         >
-          <PlayIcon
-            slot="play"
-            class=":uno: size-4 md:size-5"
-          />
-          <PauseIcon
-            slot="pause"
-            class=":uno: size-4 md:size-5"
-          />
-        </media-play-button>
+          <media-play-button
+            class=":uno: p-2.5 outline-none"
+            :aria-label="isPlaying ? 'pause' : 'play'"
+          >
+            <PlayIcon
+              slot="play"
+              class=":uno: size-4 md:size-5"
+            />
+            <PauseIcon
+              slot="pause"
+              class=":uno: size-4 md:size-5"
+            />
+          </media-play-button>
 
-        <media-time-display
-          v-if="showTimeDisplay"
-          class=":uno: min-w-10 px-0 tracking-tight outline-none md:min-w-12"
-        />
-
-        <media-time-range
-          v-if="showTimeRange"
-          class=":uno: flex-1 px-1 outline-none md:px-2.5"
-        />
-
-        <media-duration-display
-          v-if="showDurationDisplay"
-          class=":uno: min-w-12 px-0 tracking-tight outline-none"
-        />
-
-        <media-mute-button
-          v-if="showVolumeControls"
-          class=":uno: px-1 outline-none md:px-2.5"
-          :aria-label="isMuted ? 'unmute' : 'mute'"
-        >
-          <VolumeIcon
-            slot="low"
-            class=":uno: size-4 md:size-5"
+          <media-time-display
+            v-if="showTimeDisplay"
+            class=":uno: min-w-10 px-0 tracking-tight outline-none md:min-w-12"
           />
-          <Volume1Icon
-            slot="medium"
-            class=":uno: size-4 md:size-5"
-          />
-          <Volume2Icon
-            slot="high"
-            class=":uno: size-4 md:size-5"
-          />
-          <VolumeXIcon
-            slot="off"
-            class=":uno: size-4 md:size-5"
-          />
-        </media-mute-button>
 
-        <media-volume-range
-          v-if="showVolumeRange"
-          class=":uno: hidden max-w-[5.375rem] pr-1.5 outline-none sm:block"
-        />
-
-        <media-playback-rate-button
-          v-if="showPlaybackRate"
-          class=":uno: min-w-12 select-none px-0 tracking-tight outline-none"
-        />
-
-        <media-fullscreen-button
-          v-if="showFullscreenButton"
-          class=":uno: p-2.5 outline-none"
-          :aria-label="isFullscreen ? 'exit fullscreen mode' : 'enter fullscreen mode'"
-        >
-          <MaximizeIcon
-            slot="enter"
-            class=":uno: size-4 md:size-5"
+          <media-time-range
+            v-if="showTimeRange"
+            class=":uno: flex-1 px-1 outline-none md:px-2.5"
           />
-          <MinimizeIcon
-            slot="exit"
-            class=":uno: size-4 md:size-5"
+
+          <media-duration-display
+            v-if="showDurationDisplay"
+            class=":uno: min-w-12 px-0 tracking-tight outline-none"
           />
-        </media-fullscreen-button>
-      </media-control-bar>
-    </div>
-  </media-controller>
+
+          <media-mute-button
+            v-if="showVolumeControls"
+            class=":uno: px-1 outline-none md:px-2.5"
+            :aria-label="isMuted ? 'unmute' : 'mute'"
+          >
+            <VolumeIcon
+              slot="low"
+              class=":uno: size-4 md:size-5"
+            />
+            <Volume1Icon
+              slot="medium"
+              class=":uno: size-4 md:size-5"
+            />
+            <Volume2Icon
+              slot="high"
+              class=":uno: size-4 md:size-5"
+            />
+            <VolumeXIcon
+              slot="off"
+              class=":uno: size-4 md:size-5"
+            />
+          </media-mute-button>
+
+          <media-volume-range
+            v-if="showVolumeRange"
+            class=":uno: hidden max-w-[5.375rem] pr-1.5 outline-none sm:block"
+          />
+
+          <media-playback-rate-button
+            v-if="showPlaybackRate"
+            class=":uno: min-w-12 select-none px-0 tracking-tight outline-none"
+          />
+
+          <media-fullscreen-button
+            v-if="showFullscreenButton"
+            class=":uno: p-2.5 outline-none"
+            :aria-label="isFullscreen ? 'exit fullscreen mode' : 'enter fullscreen mode'"
+          >
+            <MaximizeIcon
+              slot="enter"
+              class=":uno: size-4 md:size-5"
+            />
+            <MinimizeIcon
+              slot="exit"
+              class=":uno: size-4 md:size-5"
+            />
+          </media-fullscreen-button>
+        </media-control-bar>
+      </div>
+    </media-controller>
+  </div>
 </template>
 
 <style scoped>
